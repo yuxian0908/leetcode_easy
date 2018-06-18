29e9e73c2ca5fea1dd28320b5d028f6e64594da4
yuxian <andyretajeff@gmail.com>
2018-06-18 21:07:24 +0800

    greedy

---
+++ AssignCookies.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+
+    public int findContentChildren(int[] g, int[] s) {
+    	Arrays.sort(g);
+    	Arrays.sort(s);
+    	
+    	int res = 0;
+    	
+    	for(int i=0; res<g.length&&i<s.length; i++)
+    		if(g[res]<=s[i]) res++;
+    			
+        return res;
+    }
+	
+}
---
+++ BestTimetoBuyandSellStockII.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class BestTimetoBuyandSellStockII {
+    public int maxProfit(int[] prices) {
+    	if(prices.length==0) return 0;
+    	int res = 0;
+    	for(int i=1; i<prices.length; i++) {
+    		if(prices[i]>prices[i-1])
+    			res = res + (prices[i]-prices[i-1]);
+    	}
+    	
+        return res;
+    }
+}
